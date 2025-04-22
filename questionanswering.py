e90f815f906f5a0d05dbc7da3d54ee93c7ee7403
kevin-4561 <apexlegendsmb09@gmail.com>
2025-04-22 08:02:47 +0530

    questionanswering.py

--- questionanswering.py
+++ questionanswering.py
@@ -2,12 +2,12 @@ import spacy
 
 nlp=spacy.load("en_core_web_sm")
 
-text=""
-question""
+text="Elon Musk founded SpaceX in 2002."
+question="Who founded SpaceX? "
 
 doc=nlp(text)
 
 for ent in doc.ents:
-    if question.lower().startswith("who").and ent.label=="PERSON":
+    if question.lower().startswith("who")and ent.label_=="PERSON":
         print(ent.text)
         break
